dd38724bced213bd48bcd7a6c906f5575a9cbb2d
Kingluddite <howley.phil@gmail.com>
2020-02-16 23:30:07 -0800

    Add route question

---
+++ route.js
@@ -0,0 +1,22 @@
+// This code is just a fragment of code that will not work
+// Just think about the question and discuss why it would be used
+
+app.get('/api/dogs', function(req, res) {
+  connection.query('SELECT * FROM Dogs', function(err, res) {
+    if (err) throw res.json(err);
+    res.json(data);
+  });
+});
+
+app.get('/api/dogs/:breed', function(req, res) {
+  connection.query(
+    'SELECT * FROM Dogs where breed = (?)',
+    [req.body.breed],
+    function(err, res) {
+      if (err) throw res.json(err);
+      res.json(data);
+    }
+  );
+});
+
+// after discussing move into activity 1
