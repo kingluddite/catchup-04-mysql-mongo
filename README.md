5d44a7356b0953fce7433ae728fff5a016a73c83
Kingluddite <howley.phil@gmail.com>
2020-04-24 15:39:12 -0700

    Add README with question

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-## After discussion move on to activity 1
+## Question: How might the route info in `route.js` use this in a GET route?
+
+After discussion move on to activity 1
+
+`$ git checkout 04-activity-01-start`
 
--- route.js
+++ route.js
@@ -1,18 +1,19 @@
+// QUESTION: How might the route info in `route.js` use this in a GET route?
 // This code is just a fragment of code that will not work
 // Just think about the question and discuss why it would be used
 
-app.get('/api/dogs', function(req, res) {
-  connection.query('SELECT * FROM Dogs', function(err, res) {
+app.get('/api/dogs', function (req, res) {
+  connection.query('SELECT * FROM Dogs', function (err, res) {
     if (err) throw res.json(err);
     res.json(data);
   });
 });
 
-app.get('/api/dogs/:breed', function(req, res) {
+app.get('/api/dogs/:breed', function (req, res) {
   connection.query(
     'SELECT * FROM Dogs where breed = (?)',
     [req.body.breed],
-    function(err, res) {
+    function (err, res) {
       if (err) throw res.json(err);
       res.json(data);
     }
